1ae21d952ba084e9cf06a36763dcc60cc1439333
ertmat <ertmat@gmail.com>
2017-04-05 15:15:53 +0200

    Working with Angular

--- Client/app/app.component.ts
+++ Client/app/app.component.ts
@@ -1,1 +1,9 @@
-﻿
+﻿import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "my-app",
+    templateUrl: "app.component.template.html"
+})
+
+export class AppComponent{}
---
+++ Client/app/components/category/category-form-component.ts
@@ -0,0 +1,38 @@
+﻿import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FlashMessagesServices } from "@angular2-flash-messages";
+import { ActivatedRoute } from "@angular/router";
+import { HttpCommonService } from "../../shared/services/http-common.service";
+
+@Component({
+    moduleId: module.id,
+    selector: "category-form",
+    templateUrl: "category-form-template.html",
+    providers: [HttpCommonService]
+})
+
+export class CategoryFromComponents {
+    public apiName: string
+    protected model = {};
+    protected submited = false;
+    private sub: any;
+    id: number;
+
+    constructor(private _httpCommonService: HttpCommonService,
+             private _flashMessagesServices: FlashMessagesServices,
+             private _route: ActivatedRoute)
+    {
+        this.apiName = "admin/category";
+        this.sub = _route.params.subscribe(params => {
+            this.id = +params["id"];
+            if (this.id > 0) {
+                _httpCommonService.getItem(this.apiName + "/get", this.id).subscribe(data => {
+                    this.model = data;
+                },
+                    err => {
+                        this.showError(err);
+                    });
+            }
+        })
+    }
+}
+
